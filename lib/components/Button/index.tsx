6dba5faf15e33e1edbf88a365da3dbbce64d3142
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-06-26 17:13:44 +0200

    Add blur effect to buttons

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -4,21 +4,17 @@ import { ButtonProps } from "./Button.types";
 const Button: FC<ButtonProps> = ({ children, type = "default" }) => {
   if (type == "accent") {
     return (
-      <button
-        className="bg-purple-gradient shadow-CardShadowMedium rounded-md"
-        style={{
-          boxShadow:
-            "0px 4px 5px rgba(121, 71, 247, 0.3), 0px 9px 10px rgba(121, 71, 247, 0.2), 0px 14px 32px rgba(121, 71, 247, 0.1)",
-        }}
-      >
-        {children}
+      <button className="bg-purple-gradient group relative rounded-md">
+        <div className="bg-purple-gradient rounded-md z-0 transition-opacity duration-700 group-hover:duration-300 absolute w-full h-full blur-md opacity-60 group-hover:opacity-90 top-1 " />
+        <div className="relative">{children}</div>
       </button>
     );
   }
 
   return (
-    <button className="bg-green-gradient shadow-CardShadowMedium rounded-md">
-      {children}
+    <button className="bg-green-gradient group rounded-md relative">
+      <div className="bg-green-gradient rounded-md z-0 transition-opacity duration-700 group-hover:duration-300 absolute w-full h-full blur-md opacity-60 group-hover:opacity-90 top-1 " />
+      <div className="relative">{children}</div>
     </button>
   );
 };
