a69fb4e79f6a0e9195d252176016b0506454a28e
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-07-29 10:24:21 +0200

    Remove background landing section

--- lib/modules/HomeLandingModule/index.tsx
+++ lib/modules/HomeLandingModule/index.tsx
@@ -9,12 +9,7 @@ const HomeLandingModule: React.FC<HomeLandingModuleProps> = ({
   return (
     <>
       <div className="relative pt-12 overflow-hidden">
-        <div className="w-[2000px] blur-2xl opacity-20 -z-10  h-[200px]  bottom-0 bg-purple-gradient absolute" />
-        <div className="w-[600px] blur-2xl -z-10 right-0 -translate-y-1/2 opacity-20 rounded-full  h-[500px] bg-PurpleNavy/5s00  top-0 bg-purple-gradient absolute" />
-
         <Container className="pb-32 medium:pb-48 large:min-h-[35rem]">
-          <BackgroundArt />
-
           <div className="mb-6 mt-16 max-w-[350px] small:max-w-[500px] medium:mb-11 medium:mt-24 medium:max-w-xl">
             <RichText content={title} />
           </div>
@@ -35,14 +30,6 @@ const HomeLandingModule: React.FC<HomeLandingModuleProps> = ({
         </Container>
       </div>
     </>
-  );
-};
-
-const BackgroundArt = () => {
-  return (
-    <div className="absolute top-[-100px] left-[-158px] -z-20 w-[574px] scale-150 opacity-60 small:left-0 small:scale-[200%] medium:left-[40%] medium:top-0">
-      <img src="/assets/bg-art.svg" />
-    </div>
   );
 };
 
