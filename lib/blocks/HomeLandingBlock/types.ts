c2945a1ceb8a644a1236d1d1db8496e9d9ff9b42
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-11 17:14:51 +0200

    Remove landing blok

--- lib/blocks/HomeLandingBlock/types.ts
+++
@@ -1,5 +0,0 @@
-export interface HomeLandingBlockProps {
-  title: string | JSX.Element;
-  subtitle: string | JSX.Element;
-  button: string | JSX.Element;
-}
--- lib/blocks/index.ts
+++ lib/blocks/index.ts
@@ -1,7 +1,5 @@
 export { default as AboutBlock } from "./AboutBlock";
-export { default as OffersBlock } from "./OffersBlok";
 export { default as ContactBlock } from "./ContactBlock";
-export { default as HomeLandingBlock } from "./HomeLandingBlock";
 export { default as UnqiueSellingPointsBlock } from "./UniqueSellingPointsBlock";
 export { default as HeaderBlock } from "./HeaderBlock";
 export { default as FooterBlock } from "./FooterBlock";
