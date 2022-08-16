1102285607fd9db254fba1a05f5268d173761b59
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-16 11:50:53 +0200

    Fix Spacerblok not showing in editor bug

--- lib/blocks/SpacerBlok/index.tsx
+++ lib/blocks/SpacerBlok/index.tsx
@@ -4,7 +4,7 @@ import { Blok } from "../types";
 const SpacerBlok: React.FC<{ blok: Blok }> = ({ blok }) => {
   return (
     <div
-      {...storyblokEditable}
+      {...storyblokEditable(blok)}
       style={{
         height: `${blok?.height}px`,
         backgroundColor: blok?.backgroundColor,
