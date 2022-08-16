78eb064a90f0bd7abcc809f8621ca53b72e1979e
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-16 17:11:25 +0200

    Create initial ui contact form

--- lib/blocks/ContactBlok/index.tsx
+++ lib/blocks/ContactBlok/index.tsx
@@ -1,5 +1,5 @@
 import { storyblokEditable } from "@storyblok/react";
-import { Container, CompanyCard, GradientText } from "../../components";
+import { Container, CompanyCard, GradientText, Button } from "../../components";
 import { useScreen } from "../../hooks";
 import { Blok } from "../types";
 
@@ -8,12 +8,65 @@ const GeneralContactModule: React.FC<{ blok: Blok }> = ({ blok }) => {
   return (
     <section {...storyblokEditable(blok)}>
       <Container module={blok}>
-        <h2 className="display-1 mb-10">
+        <h2 className="display-1 mb-8">
           <GradientText>{blok?.title}</GradientText>
         </h2>
-        <CompanyCard type={screen === "medium" ? "horizontal" : "verticle"} />
+        <div className="flex gap-[60px] flex-col large:flex-row">
+          <form className="flex-1 gap-3 gap-x-6 grid medium:grid-cols-2">
+            <Input placeholder="Name" name="name" />
+            <Input placeholder="Company" name="company" />
+            <Input placeholder="Telephone" name="telephone" />
+            <Input placeholder="E-mail" name="email" />
+            <Input placeholder="Catagory" name="catagory" />
+            <Input placeholder="Budget" name="budget" />
+            <div className="col-span-2">
+              <Input type="multiline" placeholder="Message" name="message" />
+            </div>
+
+            <div className="mt-8">
+              <Button type="accent">
+                <span className="body-2 whitespace-nowrap">Send Message</span>
+              </Button>
+            </div>
+          </form>
+          <div className="large:-translate-y-24">
+            <CompanyCard
+              type={screen === "medium" ? "horizontal" : "verticle"}
+            />
+          </div>
+        </div>
       </Container>
     </section>
+  );
+};
+
+interface InputProps {
+  name: string;
+  placeholder: string;
+  type?: "multiline";
+}
+
+const Input: React.FC<InputProps> = (props) => {
+  if (props.type === "multiline") {
+    return (
+      <div>
+        <textarea
+          name={props.name}
+          placeholder={props.placeholder}
+          className="border-b medium:min-h-[107px] placeholder:text-white medium:py-4 outline-none placeholder:body-2 py-3 border-dark-purple-200 w-full bg-transparent"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <input
+        name={props.name}
+        placeholder={props.placeholder}
+        className="border-b placeholder:text-white outline-none placeholder:body-2 py-3 medium:py-4 border-dark-purple-200 w-full bg-transparent"
+      />
+    </div>
   );
 };
 
--- lib/components/CompanyCard/index.tsx
+++ lib/components/CompanyCard/index.tsx
@@ -7,13 +7,10 @@ const CompanyCard: React.FC<CompanyCardProps> = ({ type = "horizontal" }) => {
   return (
     <div className="flex">
       <Card>
-        <div className="py-12 pl-6 pr-12">
-          <p className="body-3 pb-4 font-bold text-teal-500 ">
-            Virtual Friends
-          </p>
-
+        <div className="py-[30px] pl-6 pr-6 small:pr-12">
           {type == "horizontal" ? (
             <div className=" w-[600px]">
+              <p className="body-3 pb-4 font-bold ">Virtual Friends</p>
               <div className="flex justify-between">
                 <p className="body-2 mb-4 flex-1">
                   Van Nelleweg 1 <br />
@@ -45,33 +42,36 @@ const CompanyCard: React.FC<CompanyCardProps> = ({ type = "horizontal" }) => {
               </div>
             </div>
           ) : (
-            <div className="flex">
-              <div>
-                <p className="body-2 mb-4">
-                  Van Nelleweg 1 <br />
-                  3044 BC Rotterdam <br />
-                  The Netherlands
-                </p>
+            <>
+              <p className="body-3 pb-8 font-bold ">Virtual Friends</p>
+              <div className="flex">
+                <div>
+                  <p className="body-2 mb-7">
+                    Van Nelleweg 1 <br />
+                    3044 BC Rotterdam <br />
+                    The Netherlands
+                  </p>
 
-                <p className="body-2 mb-4">
-                  <span className="mr-2 text-teal-500">
-                    <FontAwesomeIcon icon={"phone"} />
-                  </span>
-                  <span>+31612818127</span> <br />
-                  <span className="mr-2 text-teal-500">
-                    <FontAwesomeIcon icon={"envelope"} />
-                  </span>
-                  <span className="ml-1">mndarwesh@virtualfriends.dev</span>
-                </p>
+                  <p className="body-2 mb-7 whitespace-nowrap">
+                    <span className="mr-2 text-teal-500">
+                      <FontAwesomeIcon icon={"phone"} />
+                    </span>
+                    <span>+31612818127</span> <br />
+                    <span className="mr-2 text-teal-500">
+                      <FontAwesomeIcon icon={"envelope"} />
+                    </span>
+                    <span className="ml-1">mndarwesh@virtualfriends.dev</span>
+                  </p>
 
-                <p className="body-2 mb-4">
-                  <span className="body-2 font-bold">KVK: </span>
-                  <span>86165321</span> <br />
-                  <span className="body-2 font-bold">BTW: </span>
-                  <span>NL291929092B02</span> <br />
-                </p>
+                  <p className="body-2">
+                    <span className="body-2 font-bold">KVK: </span>
+                    <span>86165321</span> <br />
+                    <span className="body-2 font-bold">BTW: </span>
+                    <span>NL291929092B02</span> <br />
+                  </p>
+                </div>
               </div>
-            </div>
+            </>
           )}
         </div>
       </Card>
