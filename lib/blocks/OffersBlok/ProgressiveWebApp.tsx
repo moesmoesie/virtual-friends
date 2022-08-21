4b90d0bf6339f577d8068173d8c0295e5419a28b
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-21 12:55:50 +0200

    Make pwa components float

--- lib/blocks/OffersBlok/ProgressiveWebApp.tsx
+++ lib/blocks/OffersBlok/ProgressiveWebApp.tsx
@@ -4,22 +4,33 @@ import {
   AssetBarChartCard,
   AssetLineChartCard,
 } from "../../components";
+import FloatingContainer from "../../components/FloatingContainer";
 
 const ProgressiveWebApp: React.FC = () => {
   return (
     <div className="grid relative gap-5 grid-rows-[min-content,min-content] grid-cols-[min-content,min-content,min-content]">
-      <div className="-translate-y-0">
-        <AssetCard />
-      </div>
-      <div className="translate-y-8">
-        <AssetBarChartCard />
-      </div>
-      <div className="translate-y-24">
-        <AssetCard />
-      </div>
-      <div className="col-span-3 -z-10  -translate-y-4 pr-5">
-        <AssetLineChartCard />
-      </div>
+      <FloatingContainer>
+        <div className="translate-y-2">
+          <AssetCard />
+        </div>
+      </FloatingContainer>
+      <FloatingContainer delay={0.3}>
+        <div className="translate-y-8">
+          <AssetBarChartCard />
+        </div>
+      </FloatingContainer>
+
+      <FloatingContainer delay={0.5}>
+        <div className="translate-y-20">
+          <AssetCard />
+        </div>
+      </FloatingContainer>
+
+      <FloatingContainer delay={0.7} className="col-span-3 -z-10">
+        <div className="-translate-y-4 pr-5">
+          <AssetLineChartCard />
+        </div>
+      </FloatingContainer>
     </div>
   );
 };
