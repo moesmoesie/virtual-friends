3d8ae4da7cfca7f336fe6a3163e926c6dcb232bf
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-21 13:13:40 +0200

    Make cms components float

--- lib/blocks/OffersBlok/ContentMangementSystem.tsx
+++ lib/blocks/OffersBlok/ContentMangementSystem.tsx
@@ -3,29 +3,39 @@ import { Picker } from "../../components";
 import { Button } from "../../components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TextEditor } from "../../components";
+import FloatingContainer from "../../components/FloatingContainer";
 
 const ContentMangementSystem: React.FC = () => {
   return (
     <div className="relative grid gap-6 grid-cols-[min-content,min-content]">
-      <div className="translate-x-6">
-        <div className="w-[350px] h-[200px]">
+      <FloatingContainer>
+        <div className="w-[350px] h-[200px] translate-x-6">
           <TextEditor />
         </div>
-      </div>
-      <div className="z-10 -translate-x-8 mt-24">
-        <Button type="accent">
-          <span className="px-6 py-2 flex items-center font-bold gap-2 body-1">
-            <FontAwesomeIcon className="text-teal-500 body-2" icon="plus" />
-            Deploy
-          </span>
-        </Button>
-      </div>
-      <div className="translate-x-10">
-        <UploadImageCard />
-      </div>
-      <div className="-translate-x-16">
-        <Picker />
-      </div>
+      </FloatingContainer>
+
+      <FloatingContainer delay={0.4}>
+        <div className="z-10 mt-24 translate-x-6">
+          <Button type="accent">
+            <span className="px-6 py-2 flex items-center font-bold gap-2 body-1">
+              <FontAwesomeIcon className="text-teal-500 body-2" icon="plus" />
+              Deploy
+            </span>
+          </Button>
+        </div>
+      </FloatingContainer>
+
+      <FloatingContainer delay={0.5}>
+        <div className="translate-x-10">
+          <UploadImageCard />
+        </div>
+      </FloatingContainer>
+
+      <FloatingContainer delay={0.3}>
+        <div className="-translate-x-16">
+          <Picker />
+        </div>
+      </FloatingContainer>
     </div>
   );
 };
