10d735e1b2d59a7b92f90ed65f317d61eb60c7bd
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-25 10:38:19 +0200

    Add floating icons to landing section

--- lib/blocks/HomeLandingBlok/index.tsx
+++ lib/blocks/HomeLandingBlok/index.tsx
@@ -1,15 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { storyblokEditable } from "@storyblok/react";
-import { Button, Card, Container } from "../../components";
+import { Button, Container, StoryblokImage } from "../../components";
+import FloatingContainer from "../../components/FloatingContainer";
 import { Blok } from "../types";
 
 const HomeLandingBlok: React.FC<{ blok: Blok }> = ({ blok }) => {
   return (
     <section className="relative" {...storyblokEditable(blok)}>
       <Container module={blok}>
-        <Background />
+        <Background images={blok?.landingImages ?? []} />
         <div className="flex items-center relative justify-between">
-          <div className="max-w-3xl flex flex-col gap-10">
+          <div className="max-w-2xl flex flex-col gap-10">
             <h1 className="display-1 ">{blok?.title}</h1>
             <p className="body-3 medium:max-w-[80%]">{blok.body}</p>
             <div className="flex items-center">
@@ -46,11 +47,31 @@ const HomeLandingBlok: React.FC<{ blok: Blok }> = ({ blok }) => {
   );
 };
 
-const Background: React.FC = () => {
+const Background: React.FC<{ images: any }> = (props) => {
   return (
     <>
       <div className="absolute hidden medium:block rounded-3xl -left-16 bottom-0 origin-left opacity-50 -rotate-[30deg] large:-rotate-[20deg] w-[130%] large:w-[110%] blur-3xl h-[250px] large:h-[300px] bg-gradient-1" />
       <div className="absolute medium:hidden -left-16 bottom-0 origin-left opacity-50 -rotate-[45deg] large:-rotate-[20deg] w-[200%] large:w-[110%] blur-3xl h-[250px] large:h-[300px] bg-gradient-1" />
+      <div className="w-[500px]  h-[500px] -translate-y-12 right-0 absolute">
+        {props.images.map((image: any) => {
+          const { left, top, bottom, right, translate } = image;
+          return (
+            <FloatingContainer delay={0.4}>
+              <StoryblokImage
+                style={{
+                  left,
+                  top,
+                  bottom,
+                  right,
+                  translate,
+                }}
+                className="absolute w-[300px]"
+                {...image.Image}
+              />
+            </FloatingContainer>
+          );
+        })}
+      </div>
     </>
   );
 };
