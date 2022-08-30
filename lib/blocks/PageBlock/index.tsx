14b425cad6707ab5d9fc3fb785c8ffdecadce73b
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-30 09:04:41 +0200

    Remove page fade in

--- lib/blocks/PageBlock/index.tsx
+++ lib/blocks/PageBlock/index.tsx
@@ -3,22 +3,16 @@ import { useScreen } from "../../hooks";
 import PageBlockProps from "./types";
 
 const PageBlock: React.FC<PageBlockProps> = (props) => {
-  const screen = useScreen();
-
   return (
     <>
       <Head>
-        <title>{props?.title ?? "Virtual asdasd"}</title>
+        <title>{props?.title ?? "Virtual Friends"}</title>
         {props.description && (
           <meta name="description" content={props.description} />
         )}
       </Head>
       <div className="w-full min-h-[100vh] bg-dark-purple-500 text-white">
-        <div
-          className={`transition-opacity delay-100 duration-500 ${
-            screen ? "opacity-100" : "opacity-0"
-          }`}
-        >
+        <div>
           {props.header}
           {props.children}
           {props.footer}
