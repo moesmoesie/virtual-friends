f5b43f998d4693239d2ba25d7e76511cde134af3
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-08-30 14:17:21 +0200

    Update token logic

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,18 +1,16 @@
+if(!process.env.PREVIEW_SECRET_TOKEN){
+  throw("Please provide the preview token!")
+}
+
 export default async function preview(req, res) {
   const { slug = "" } = req.query;
-  // get the storyblok params for the bridge to work
-  const params = req.url.split("?");
  
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (req.query.secret !== "MY_SECRET_TOKEN") {
+  if (req.query.secret !== process.env.PREVIEW_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
  
-  // Enable Preview Mode by setting the cookies
   res.setPreviewData({});
  
-  // Set cookie to None, so it can be read in the Storyblok iframe
   const cookies = res.getHeader("Set-Cookie");
   res.setHeader(
     "Set-Cookie",
@@ -22,5 +20,5 @@ export default async function preview(req, res) {
   );
  
   // Redirect to the path from entry
-  res.redirect(`/${slug}?${params[1]}`);
+  res.redirect(`/${slug}`);
 }
