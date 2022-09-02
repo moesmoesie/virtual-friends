735c1475307c7f219454e24cee12593976c362a9
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-09-02 13:59:45 +0200

    Add google an4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../lib/styles/globals.css";
 import type { AppProps } from "next/app";
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 import { storyblokComponents } from "../lib/blocks";
+import Script from "next/script";
 
 storyblokInit({
   accessToken: "ldAsR9RjspvNkyKDK6qQrgtt",
@@ -10,7 +11,33 @@ storyblokInit({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y2D5NZRBTD"
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y2D5NZRBTD');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y2D5NZRBTD"></script>
+// <script>
+// window.dataLayer = window.dataLayer || [];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+
+// gtag('config', 'G-Y2D5NZRBTD');
+// </script>
