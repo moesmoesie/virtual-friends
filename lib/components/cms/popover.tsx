e12437ddd360f197d51583c2d5bca2cdee536de5
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-09-18 08:32:36 +0200

    Create popover component

---
+++ lib/components/cms/popover.tsx
@@ -0,0 +1,34 @@
+export interface OptionType {
+  name: string;
+  value: string;
+}
+
+export interface PopoverType {
+  options: OptionType[];
+  currentOptionIndex: number;
+  setOption: (name: string) => void;
+}
+
+export const Popover: React.FC<PopoverType> = (props) => {
+  return (
+    <div className="inline-flex flex-col rounded-lg bg-neutral-400 py-3 shadow-m">
+      {props.options.map((el) => {
+        const isActive =
+          props.options[props.currentOptionIndex].value === el.value;
+        return (
+          <button
+            onClick={() => props.setOption(el.name)}
+            className="body-3 flex items-center justify-between px-2 py-1.5"
+          >
+            <span className="pr-6 text-black">{el.name}</span>
+            <div
+              className={`h-3 w-3 rounded-full ${
+                isActive ? "bg-primary-400" : "bg-black"
+              }`}
+            />
+          </button>
+        );
+      })}
+    </div>
+  );
+};
