2346164bf74ec98c8878e99866fcdd393365c598
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-09-24 14:32:32 +0200

    Create container component

---
+++ apps/storybook/stories/container.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Container as Component } from "ui";
+
+export default {
+  title: "Container",
+  component: Component,
+} as ComponentMeta<typeof Component>;
+
+const Template: ComponentStory<typeof Component> = (args) => (
+  <Component {...args} />
+);
+
+export const Container = Template.bind({});
+Container.args = {
+  backgroundColor: "black",
+  padding: "100px 100px",
+};
