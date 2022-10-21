ce5fee42a657e59ce6494554a6050c8a27c05d4d
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-10-22 01:57:08 +0200

    Update remote url

--- studio/src/resolveProductionUrl.js
+++ studio/src/resolveProductionUrl.js
@@ -2,7 +2,7 @@
 const previewSecret = process.env.SANITY_STUDIO_PREVIEW_SECRET;
 
 // Replace `remoteUrl` with your deployed Next.js site
-const remoteUrl = `https://code-to-inspire-blue.vercel.app`;
+const remoteUrl = `https://www.virtualfriends.dev`;
 const localUrl = `http://localhost:3000`;
 
 export default function resolveProductionUrl(doc) {
