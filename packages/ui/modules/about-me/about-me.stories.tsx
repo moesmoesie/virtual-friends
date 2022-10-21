83f6bea5f0bf402dde6b5993dbe550eb619fdee3
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-10-21 20:18:37 +0200

    Rename module

--- packages/ui/modules/about-me/about-me.stories.tsx
+++ packages/ui/modules/about-me/about-me.stories.tsx
@@ -1,10 +1,9 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { AboutMe as Component } from "..";
 import React from "react";
-import { GradientText } from "../../components";
 
 export default {
-  title: "Modules/Amout Me",
+  title: "Modules/About Me",
   component: Component,
 } as ComponentMeta<typeof Component>;
 
