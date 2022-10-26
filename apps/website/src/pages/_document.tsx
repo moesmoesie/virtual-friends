25a6ea9644285fc888d5b6e8957a73beab845e65
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-10-26 23:01:17 +0200

    Preload fonts

---
+++ apps/website/src/pages/_document.tsx
@@ -0,0 +1,68 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+
+type Props = {};
+
+class Document extends NextDocument<Props> {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter-v11-latin/inter-v11-latin-700.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          <link
+            rel="preload"
+            href="/fonts/inter-v11-latin/inter-v11-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          <link
+            rel="preload"
+            href="/fonts/inter-v11-latin/inter-v12-latin-900.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          <link
+            rel="preload"
+            href="/fonts/source-sans-pro-v21-latin/source-sans-pro-v21-latin-300.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          <link
+            rel="preload"
+            href="/fonts/source-sans-pro-v21-latin/source-sans-pro-v21-latin-700.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+
+          <link
+            rel="preload"
+            href="/fonts/source-sans-pro-v21-latin/source-sans-pro-v21-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
