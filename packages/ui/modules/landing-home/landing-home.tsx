667886edbb35cfd6166ffbfa89c9df8b7698e4b0
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-10-30 16:42:33 +0100

    Add framer motion animation

--- packages/ui/modules/landing-home/landing-home.tsx
+++ packages/ui/modules/landing-home/landing-home.tsx
@@ -8,7 +8,8 @@ import {
   RichText,
   ButtonType,
 } from "../../components";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export interface LandingHomeType extends ModuleContainerType {
   title: string | JSX.Element;
@@ -24,27 +25,30 @@ export const LandingHome: React.FC<LandingHomeType> = (props) => {
     <ModuleContainer module={props?.module}>
       <Container>
         <div className="text-center min-h-[80vh] large:text-left flex flex-col items-center justify-center gap-12 large:flex-row">
-          <div className="rounded-full relative overflow-hidden medium:w-[250px] medium:h-[250px] large:w-[320px] large:h-[320px] object-cover w-[200px] h-[200px]">
-            <div className="absolute left-0 top-0 h-full w-full">
-              <div className="w-full relative h-full">
-                <Image
-                  withPlaceholder={true}
-                  lazy={false}
-                  className="w-full h-full absolute top-0 left-0 object-cover pointer-events-none"
-                  {...props?.imageBackground}
-                  width="600"
-                />
-              </div>
-            </div>
+          <motion.div
+            initial={{ y: 64, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="rounded-full relative overflow-hidden medium:w-[250px] medium:h-[250px] large:w-[320px] large:h-[320px] object-cover w-[200px] h-[200px]"
+          >
+            <motion.div className="w-full rounded-full overflow-hiddens relative h-full">
+              <Image
+                withPlaceholder={true}
+                lazy={false}
+                className="w-full h-full absolute top-0 left-0 object-cover pointer-events-none"
+                {...props?.imageBackground}
+                width="600"
+              />
 
-            <Image
-              lazy={false}
-              className="absolute left-0 scale-95 origin-bottom top-0 w-full h-full object-cover"
-              {...props?.image}
-              width="600"
-              alt="Headshot of Mustafa"
-            />
-          </div>
+              <Image
+                lazy={false}
+                className={`scale-95 origin-bottom w-full h-full z-50 object-cover transition-transform`}
+                {...props?.image}
+                width="600"
+                alt="Headshot of Mustafa"
+              />
+            </motion.div>
+          </motion.div>
 
           <div className="flex flex-col max-w-md medium:max-w-2xl gap-8 items-center large:items-start justify-center">
             <h1 className="headline-4 medium:headline-2 large:headline-1">
