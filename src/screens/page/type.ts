46e9dc9746e08acd0517fc285381ca70d09de59a
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-11-20 19:24:19 +0100

    Fix seo not loading bug

--- src/screens/page/type.ts
+++ src/screens/page/type.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { SeoZod } from "../../components/seo/seo";
+import { fallback } from "../../helpers/zod";
 import { AboutMeZod } from "../../modules/about-me/type";
 import { ArticleZod } from "../../modules/article/type";
 import { ContactZod } from "../../modules/contact/type";
@@ -13,7 +14,7 @@ import { SpacerZod } from "../../modules/spacer/type";
 export const ModuleZod = z.union([AboutMeZod, SolutionsZod, LandingHomeZod, HeaderZod, ContactZod, FooterZod, SellingPointsZod, SpacerZod, ArticleZod]);
 
 export const PageZod = z.object({
-  seo: SeoZod.optional(),
+  seo: SeoZod.or(fallback(null)),
   modules: z.array(ModuleZod),
 });
 
