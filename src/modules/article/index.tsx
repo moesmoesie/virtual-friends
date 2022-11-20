f04b10e0f42043abc8bb5ee6355eb20d294c7875
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-11-20 19:36:38 +0100

    Fix markdown loading bug

--- src/modules/article/index.tsx
+++ src/modules/article/index.tsx
@@ -1,8 +1,8 @@
-import Markdown from "markdown-to-jsx";
 import React from "react";
 import Container from "../../components/container/container";
 import ModuleContainer from "../../components/module-container/module-container";
 import { ArticleType } from "./type";
+import { Markdown } from "../../components/markdown";
 
 export const Article: React.FC<ArticleType> = (props) => {
   return (
