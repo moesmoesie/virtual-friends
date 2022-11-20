e007a25ea7744305e5baf74bba6c2dffceca8b08
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-11-20 17:42:10 +0100

    Add eth icon to asset card in saas section

--- src/modules/solutions/components/saas/index.tsx
+++ src/modules/solutions/components/saas/index.tsx
@@ -5,6 +5,8 @@ import { Floating } from "../../../../components/floating/floating";
 import { Solution } from "../content/content";
 import { SolutionType } from "../../type";
 
+// TODO MAKE COMPONENTS MORE INTERACTIVE PERHAPS USE D3 FOR THIS
+
 export const SolutionSaas: React.FC<SolutionType> = (props) => {
   return (
     <Solution content={props.content} isReversed={props.isReversed}>
@@ -13,7 +15,7 @@ export const SolutionSaas: React.FC<SolutionType> = (props) => {
           <div className="">
             <Floating delay={1}>
               <div className="translate-y-14">
-                <AssetCard percentage="+23%" value="52,7K" Icon={<BitCoinIcon />} />
+                <AssetCard percentage="+23%" value="52,7K" Icon={<BTC />} />
               </div>
             </Floating>
           </div>
@@ -25,7 +27,7 @@ export const SolutionSaas: React.FC<SolutionType> = (props) => {
 
           <Floating>
             <div className="-translate-y-2">
-              <AssetCard percentage="+23%" value="52,7K" Icon={<BitCoinIcon />} />
+              <AssetCard percentage="+11%" value="22,7K" Icon={<ETH />} />
             </div>
           </Floating>
         </div>
@@ -41,10 +43,18 @@ export const SolutionSaas: React.FC<SolutionType> = (props) => {
   );
 };
 
-const BitCoinIcon = () => {
+const BTC = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-bitcoin">
-      <path d="M5 13h14M12 3v3M12 18v3M7 8l5 5 5-5"></path>
+    <svg width={14} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+      <path d="M310.204 242.638c27.73-14.18 45.377-39.39 41.28-81.3-5.358-57.351-52.458-76.573-114.85-81.929V0h-48.528v77.203c-12.605 0-25.525.315-38.444.63V0h-48.528v79.409c-17.842.539-38.622.276-97.37 0v51.678c38.314-.678 58.417-3.14 63.023 21.427v217.429c-2.925 19.492-18.524 16.685-53.255 16.071L3.765 443.68c88.481 0 97.37.315 97.37.315V512h48.528v-67.06c13.234.315 26.154.315 38.444.315V512h48.528v-68.005c81.299-4.412 135.647-24.894 142.895-101.467 5.671-61.446-23.32-88.862-69.326-99.89zM150.608 134.553c27.415 0 113.126-8.507 113.126 48.528 0 54.515-85.71 48.212-113.126 48.212v-96.74zm0 251.776V279.821c32.772 0 133.127-9.138 133.127 53.255-.001 60.186-100.355 53.253-133.127 53.253z" />
+    </svg>
+  );
+};
+
+const ETH = () => {
+  return (
+    <svg width={14} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+      <path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z" />
     </svg>
   );
 };
