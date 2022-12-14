2ee4e97b2e98931e1495ccdef544c13e52e1e2dd
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-12-14 18:47:02 +0100

    Change some stuff

--- src/modules/landing-home/index.tsx
+++ src/modules/landing-home/index.tsx
@@ -13,18 +13,6 @@ export const LandingHome: React.FC<LandingHomeType> = (props) => {
     <ModuleContainer module={props?.module}>
       <Container>
         <div className="text-center min-h-[80vh] large:text-left flex flex-col items-center justify-center gap-12 large:flex-row">
-          <motion.div
-            initial={{ y: 64, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-            className="rounded-full relative overflow-hidden medium:w-[250px] medium:h-[250px] large:w-[320px] large:h-[320px] object-cover w-[200px] h-[200px]"
-          >
-            <motion.div className="w-full rounded-full overflow-hiddens relative h-full">
-              {props.imageBackground && <Image {...props?.imageBackground} width={400} priority className="w-full h-full absolute top-0 left-0 object-cover pointer-events-none" />}
-              {props.image && <Image loader={getCustomImageLoader} {...props.image} width={200} priority className="w-full absolute bottom-0" />}
-            </motion.div>
-          </motion.div>
-
           <div className="flex flex-col max-w-md medium:max-w-2xl gap-8 items-center large:items-start justify-center">
             <h1 className="headline-4 medium:headline-2 large:headline-1">
               <RichText value={props.title} />
